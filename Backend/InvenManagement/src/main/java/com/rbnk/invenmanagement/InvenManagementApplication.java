22252798885c8c8c2779c347cc9f51e4851f8040
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-02-14 12:42:06 +0300

    dependencies configured

---
+++ Backend/InvenManagement/src/main/java/com/rbnk/invenmanagement/InvenManagementApplication.java
@@ -0,0 +1,13 @@
+package com.rbnk.invenmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvenManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvenManagementApplication.class, args);
+    }
+
+}
