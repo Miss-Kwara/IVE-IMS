1f5767969e30b9e14c9e8643da5f9bb2845f01f6
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-02-15 14:17:07 +0300

    refined class

--- Backend/InvenManagement/src/main/java/com/rbnk/invenmanagement/entity/Booking.java
+++ Backend/InvenManagement/src/main/java/com/rbnk/invenmanagement/entity/Booking.java
@@ -36,4 +36,71 @@ public class Booking {
     protected void onCreate() {
         createdAt = LocalDateTime.now();
     }
+
+    public Booking() {
+    }
+
+    public Booking(Equipment equipment, User booker, LocalDateTime startTime, LocalDateTime endTime, String status) {
+        this.equipment = equipment;
+        this.booker = booker;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Equipment getEquipment() {
+        return equipment;
+    }
+
+    public void setEquipment(Equipment equipment) {
+        this.equipment = equipment;
+    }
+
+    public User getBooker() {
+        return booker;
+    }
+
+    public void setBooker(User booker) {
+        this.booker = booker;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
